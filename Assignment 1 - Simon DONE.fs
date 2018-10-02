10007cfcf6ef021a888bbb35687804fe7493ea26
DeterjoSimon <43176887+DeterjoSimon@users.noreply.github.com>
2018-10-03 00:14:41 +0200

    Assgn1 done

---
+++ Assignment 1 - Simon DONE.fs
@@ -0,0 +1,64 @@
+type Species = string
+type Location = string
+type Time = int
+type Observation = Species*Location*Time
+let os = [("Owl","L1",3);("Sparrow","L2",4);("Eagle","L3",5);("Falcon","L2",7);("Sparrow","L1",9);("Eagle","L1",14)]
+//Task 2
+
+//1) locationsof: Species -> Obsrvation list -> Location list
+
+let rec locationsOf s = function
+    | [] -> []
+    | (sp,lc,_)::ys when sp=s -> lc::locationsOf s ys
+    | x::ys -> locationsOf s ys
+
+//2) 
+type Count<'a when 'a:equality> = ('a*int) list
+
+
+let rec insert a = function
+    | (sp,y)::ys -> if sp=a then (sp,y+1)::ys 
+                    else (sp,1)::insert a ys
+    | [] -> [(a,1)]
+
+//Tests
+insert 'b' [('a',1);('b',2)];;
+insert 'a' [('a',1);('b',2)];;
+insert 1 [('a',1);('b',2)];;
+
+//3) Observation list -> Count<Species>
+
+let rec toCount l =
+    match l with
+    | (sp,_,_)::ys -> insert sp (toCount ys)
+    | [] -> []
+
+//4) 
+type Interval = Time*Time
+
+let rec select f intv os = 
+    match (intv,os) with
+    | ((t1,t2),(sp,lo,t)::ys) -> if t1<=t && t2>=t
+                                 then f (sp,lo) :: select f intv ys
+                                 else select f intv ys
+    | (_,[]) -> []
+
+//5) 
+
+let e = select (fun x -> x) (4,9) os;;
+
+//Task 3
+
+//1)
+
+(* let rec locationsOf s = function
+    | [] -> []
+    | (sp,lc,_)::ys when sp=s -> lc::locationsOf s ys
+    | x::ys -> locationsOf s ys
+*)
+let locationsOf s os = List.map(fun (a,b,c) -> b) (List.filter (fun (sp,l,t) -> sp=s) os)
+
+//3) 
+
+
+let toCount2 l = List.foldBack insert (List.map (fun (sp,l,t) -> sp) l) []
